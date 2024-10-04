5de6cd023ab661835561e4864b6a137e7f69e750
ujizin <lucasyujideveloper@gmail.com>
2024-10-04 21:45:08 +0100

    chore(main): add AndroidEntryPoint annotation to MainActivity

--- app/src/main/java/com/ujizin/catchallenge/MainActivity.kt
+++ app/src/main/java/com/ujizin/catchallenge/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.ujizin.catchallenge.core.navigation.CatChallengeNavigation
 import com.ujizin.catchallenge.core.ui.theme.CatChallengeTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

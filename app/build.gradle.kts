3927eabf3cbc18e08801f2cb5339360ad3cc76ae
ujizin <lucasyujideveloper@gmail.com>
2024-10-06 22:13:58 +0100

    build: add kover test coverage into project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
     alias(libs.plugins.kotlinx.serialization)
+    alias(libs.plugins.kover)
 }
 
 val localProperties = Properties()
@@ -95,4 +96,38 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.ui.test.junit4)
+}
+
+kover {
+    reports.filters.excludes {
+        androidGeneratedClasses()
+        classes(
+            "*_MembersInjector",
+            "*Module_*Factory",
+            "*Factory*.*",
+            "**.di.**",
+            "*_Impl",
+            "dagger.hilt.*",
+            "hilt_aggregated_deps.*",
+            "*_Factory",
+            "*_Factory\$*",
+            "*_*Factory",
+            "*_*Factory\$*",
+            "*.Hilt_*",
+            "*_HiltModules",
+            "*_HiltModules\$*",
+            "*_Impl",
+            "*_Impl\$*",
+            "**.*Module*.*",
+            "**.BuildConfig.*",
+            "**.ComposableSingletons*.*",
+            "**.ComposableSingletons\$*",
+            "**.ui.preview.**",
+            "**.ui.themes.**",
+            "**.ui.utils.**",
+            "**.core.navigation.**",
+            "**.ui.**",
+        )
+        annotatedBy("androidx.compose.ui.tooling.preview.Preview")
+    }
 }

3c9b2a6c4db0ce39aa05b1062f408227f1647649
ujizin <lucasyujideveloper@gmail.com>
2024-10-06 15:05:34 +0100

    chore(ui): add rememberSaveable to coil extension

--- app/src/main/java/com/ujizin/catchallenge/core/ui/utils/CoilExtensions.kt
+++ app/src/main/java/com/ujizin/catchallenge/core/ui/utils/CoilExtensions.kt
@@ -3,7 +3,7 @@ package com.ujizin.catchallenge.core.ui.utils
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import coil.request.ImageRequest
 
@@ -11,7 +11,7 @@ import coil.request.ImageRequest
 fun ImageRequest.Builder.dataImageSuffixFormat(
     imageUrl: String?,
 ): ImageRequest.Builder = run {
-    var format by remember { mutableStateOf("jpg") }
+    var format by rememberSaveable { mutableStateOf("jpg") }
     data("$imageUrl.$format")
         .listener(
             onError = { _, _ -> if (format != "png") format = "png" }

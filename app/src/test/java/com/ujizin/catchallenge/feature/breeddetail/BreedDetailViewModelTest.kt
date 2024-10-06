46e6e4e1237da0694d4a7eabf58b84e8092389c2
ujizin <lucasyujideveloper@gmail.com>
2024-10-06 23:04:51 +0100

    test(breed-detail): add test to breed detail feature

---
+++ app/src/test/java/com/ujizin/catchallenge/feature/breeddetail/BreedDetailViewModelTest.kt
@@ -0,0 +1,119 @@
+package com.ujizin.catchallenge.feature.breeddetail
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.navigation.toRoute
+import com.ujizin.catchallenge.core.data.repository.BreedRepository
+import com.ujizin.catchallenge.core.data.repository.model.Breed
+import com.ujizin.catchallenge.core.navigation.destination.Destination.BreedDetail
+import com.ujizin.catchallenge.core.navigation.destination.Destination.BreedDetail.Companion.typeMap
+import com.ujizin.catchallenge.core.test.MainCoroutineRule
+import com.ujizin.catchallenge.feature.BreedFeatureModelUtils.createBreedList
+import com.ujizin.catchallenge.feature.breeddetail.ui.BreedDetailUIEvent
+import com.ujizin.catchallenge.feature.home.ui.mapper.toBreedUI
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.mockkStatic
+import io.mockk.unmockkStatic
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class BreedDetailViewModelTest {
+
+    @get:Rule
+    val mainCoroutineRule = MainCoroutineRule()
+
+    @MockK
+    private lateinit var mockBreedRepository: BreedRepository
+
+    @RelaxedMockK
+    private lateinit var mockSavedStateHandle: SavedStateHandle
+
+    private lateinit var sutViewModel: BreedDetailViewModel
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        // FIXME https://issuetracker.google.com/issues/349807172?pli=1
+        mockkStatic(SAVED_STATE_HANDLE_PACKAGE_NAME)
+    }
+
+    @After
+    fun tearsDown() {
+        unmockkStatic(SAVED_STATE_HANDLE_PACKAGE_NAME)
+    }
+
+    @Test
+    fun `given detail breed, when starts, then should retrieve from route`() {
+        // Given
+        val breedUI = createBreedList().random().toBreedUI()
+        every { mockSavedStateHandle.toRoute<BreedDetail>(typeMap) } returns BreedDetail(breedUI)
+
+        // When
+        createSut()
+
+        // Then
+        val value = sutViewModel.uiState.value
+        assertEquals(breedUI, value.breed)
+    }
+
+    @Test
+    fun `given favorite, when updated, then should call repository and update state`() = runTest {
+        // Given
+        val breedUI = createBreedList().random().toBreedUI()
+        val expectedIsFavorite = !breedUI.isFavorite
+
+        every { mockSavedStateHandle.toRoute<BreedDetail>(typeMap) } returns BreedDetail(breedUI)
+
+        coEvery {
+            mockBreedRepository.updateFavorite(breedUI.id, expectedIsFavorite)
+        } returns flowOf(true)
+
+        createSut()
+
+        // When
+        sutViewModel.onEvent(BreedDetailUIEvent.OnFavoriteChanged(expectedIsFavorite))
+
+        // Then
+        coVerify {
+            mockBreedRepository.updateFavorite(breedUI.id, expectedIsFavorite)
+        }
+
+        val uiState = sutViewModel.uiState.first()
+
+        assertEquals(expectedIsFavorite, uiState.breed.isFavorite)
+    }
+
+    @Test
+    fun `given detail breed, when back pressed, then should do nothing`() {
+        // Given
+        val breedUI = createBreedList().random().toBreedUI()
+        every { mockSavedStateHandle.toRoute<BreedDetail>(typeMap) } returns BreedDetail(breedUI)
+
+        createSut()
+
+        // When
+        sutViewModel.onEvent(BreedDetailUIEvent.BackPressed)
+
+        // Then
+        coVerify(exactly = 0) { mockBreedRepository.updateFavorite(any(), any()) }
+    }
+
+    private fun createSut() {
+        sutViewModel = BreedDetailViewModel(mockBreedRepository, mockSavedStateHandle)
+    }
+
+    companion object {
+        private const val SAVED_STATE_HANDLE_PACKAGE_NAME = "androidx.navigation.SavedStateHandleKt"
+    }
+}
